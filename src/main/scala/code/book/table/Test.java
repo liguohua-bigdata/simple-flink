dbc41447adc3c909ae19a61a9cea3a6e04f3bad9
liguohua <2818461312@qq.com>
2016-12-11 01:43:52 +0800

    first commit

--- src/main/scala/code/book/table/Test.java
+++ src/main/scala/code/book/table/Test.java
@@ -6,6 +6,8 @@ import org.apache.flink.api.java.table.BatchTableEnvironment;
 import org.apache.flink.api.table.Table;
 import org.apache.flink.api.table.TableEnvironment;
 import org.apache.flink.api.table.Types;
+import org.apache.flink.api.table.sinks.CsvTableSink;
+import org.apache.flink.api.table.sinks.TableSink;
 import org.apache.flink.api.table.sources.CsvTableSource;
 
 /**
@@ -15,14 +17,17 @@ public class Test {
     public static void main(String[] args) {
         try {
             ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
             BatchTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(env);
             String path="/Users/liguohua/Documents/F/code/idea/git/simple-flink/src/main/scala/code/book/table/teacher";
             CsvTableSource csvTableSource = new CsvTableSource(path, new String[] { "tname", "taddr" },
                     new TypeInformation<?>[] {  Types.STRING(), Types.STRING() });
             tableEnv.registerTableSource("teacher", csvTableSource);
             Table tab = tableEnv.scan("teacher");
+            String patho="/Users/liguohua/Documents/F/code/idea/git/simple-flink/src/main/scala/code/book/table/teacher";
 
-
+            TableSink<?> sink = new CsvTableSink(patho, "|");
+            tab .writeToSink(sink);
 //            DataSet<Teacher> ds = tableEnv.toDataSet(tab, Teacher.class);
 //
 //            tableEnv.registerDataSet("user2", ds, "trans_id,part_dt,lstg_format_name,leaf_categ_id,lstg_site_id,slr_segment_cd,price,item_count,seller_id");
@@ -36,7 +41,7 @@ public class Test {
 //            // execute the program
 //            env.setParallelism(1);
             env.execute("Flink Sales SUM");
-//            org.apache.flink.api.table.sources.BatchTableSource
+
         } catch (Exception e) {
             e.printStackTrace();
         }
